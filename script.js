f3d755766ebc3ba41e1c96d5ac80d0456c2d6e4d
GREGORIOtsr <gralfonsotr@gmail.com>
2023-10-10 12:09:24 +0200

    finished validation

--- script.js
+++ script.js
@@ -3,31 +3,20 @@ const startSection = document.getElementById('startSection');
 const quizForm = document.getElementById('quizForm');
 const resultSection = document.getElementById('resultSection');
 const restart = document.getElementById('restart');
-const questions = document.querySelectorAll('#quizForm h2');
+const questionH2 = document.querySelectorAll('#quizForm h2');
 const radioButton = document.getElementsByClassName('radioButton');
 
 // Variables respuestas
 const quizAnswers = [1, 0, 0, 3, 2, 0, 2, 1, 3, 2];
 const rightAnswers = ['answer1b', 'answer2a', 'answer3a', 'answer4d', 'answer5c', 'answer6a', 'answer7c', 'answer8b', 'answer9d', 'answer10c']
-let notAnswered = [];
-
+let notAnswered = '';
+let answered = 0;
 
 
 // Funciones
 // Función para botón que devuelva al principio de la página.
 function backToTop() {
     scrollTo(top);
-}
-
-// Función para validar que cada pregunta tenga respuesta?
-function checked(input) {
-    for (i = 0; i < input.length; i++) {
-        if (this.checked && input[i].name == `answer${i+1}`) {
-            return true;
-        }
-        notAnswered.push();
-    }
-    return false;
 }
 
 // Función para comparar valor con cada valor de un array
@@ -47,6 +36,8 @@ function disableAll(arr) {
     }
 }
 
+
+// Eventos
 // Evento para ocultar sección de inicio a quiz.
 startSection.addEventListener('click', function(event) {
     this.style = "display:none";
@@ -79,8 +70,33 @@ for (i = 0; i < rightAnswers.length; i++) {
 quizForm.addEventListener('submit', function(event) {
     event.preventDefault();
 
-    if (checked(radioButton) != false) {
+    for (i = 0; i < rightAnswers.length; i++) {
+        let question = document.getElementsByName('answer'+(i+1));
+        if (question[0].checked) {
+            answered += 1;
+        } else if (question[1].checked) {
+            answered += 1;
+        } else if (question[2].checked) {
+            answered += 1;
+        } else if (question[3].checked) {
+            answered += 1;
+        } else {notAnswered += `${questionH2[i].innerHTML.slice(0,2)}\n`}
+    }
+
+    if (answered == 10) {
+        // Resultado
         let result = 0;
+
+        /* for (i = 0; i < rightAnswers.length; i++) {
+            let test = document.getElementsByName('answer'+(i+1))
+            for (j = 0; j < test.length; j++) {
+                if (test[j].value == rightAnswers[i]) {
+                    result += 1;
+                console.log('Right answer');
+                } else {console.log('Wrong answer');}
+            }
+        } */
+
         for (i = 0; i < quizAnswers.length; i++) {
             let answer = document.getElementsByName(`answer${i+1}`);
             if (answer[quizAnswers[i]].checked) {
@@ -88,14 +104,16 @@ quizForm.addEventListener('submit', function(event) {
                 console.log('Right answer');
             } else {console.log('Wrong answer');}
         }
+
         document.querySelector('#result p').innerHTML = `${result} / 10`;
         scrollTo(top);
         quizForm.style = "display:none";
         resultSection.setAttribute('class', 'appearAni');
         resultSection.style = "display:''";
-    } else {
-        scrollTo(top);
-        alert(`¡Error! No has respondido las preguntas: 
-                ${notAnswered}`);
+        } else {
+            answered = 0;
+            scrollTo(top);
+            alert(`¡Error! No has respondido las preguntas: 
+${notAnswered}`);
     }
 });
