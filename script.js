7d51861659bf950dcd0e42f5807cb35242d766bd
GREGORIOtsr <gralfonsotr@gmail.com>
2023-10-10 13:49:35 +0200

    add/audio button + event/optimized validation

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// Variables document
+// Variables
 const startSection = document.getElementById('startSection');
 const quizForm = document.getElementById('quizForm');
 const resultSection = document.getElementById('resultSection');
 const restart = document.getElementById('restart');
 const questionH2 = document.querySelectorAll('#quizForm h2');
 const radioButton = document.getElementsByClassName('radioButton');
+const topButton = document.getElementById('backToTop');
+const audioButton = document.getElementById('audioButton');
+const jigglypuff = document.querySelector('#audioButton img');
 
-// Variables respuestas
-const quizAnswers = [1, 0, 0, 3, 2, 0, 2, 1, 3, 2];
+const audio = document.getElementById('littlerootTown');
+// audio.volume = 0.15;
+audio.volume = 0;
+
 const rightAnswers = ['answer1b', 'answer2a', 'answer3a', 'answer4d', 'answer5c', 'answer6a', 'answer7c', 'answer8b', 'answer9d', 'answer10c']
 let notAnswered = '';
 let answered = 0;
 
 
 // Funciones
-// Función para botón que devuelva al principio de la página.
-function backToTop() {
-    scrollTo(top);
-}
-
 // Función para comparar valor con cada valor de un array
 function compare(value, arr) {
     for (i = 0; i < arr.length; i++) {
@@ -37,8 +37,28 @@ function disableAll(arr) {
 }
 
 
+
 // Eventos
-// Evento para ocultar sección de inicio a quiz.
+// Evento para botón que activa/desactiva audio y cambia imagen del botón de audio.
+let flag = true;
+const unmuted = './assets/sprites/jigglypuff.gif';
+const muted = './assets/sprites/jigglypuffMuted.png';
+audioButton.addEventListener('click', function(event) {
+    audio.muted = !audio.muted;
+    if (flag) {
+        jigglypuff.src = muted
+    } else {
+        jigglypuff.src = unmuted
+    }
+    flag = !flag;
+})
+
+// Evento para botón que scrollee al principio de la página (Back to top).
+topButton.addEventListener('click', function(event) {
+    scrollTo(top);
+})
+
+// Evento para ocultar sección de inicio y mostrar quiz.
 startSection.addEventListener('click', function(event) {
     this.style = "display:none";
     quizForm.setAttribute('class', 'appearAni');
@@ -83,37 +103,31 @@ quizForm.addEventListener('submit', function(event) {
         } else {notAnswered += `${questionH2[i].innerHTML.slice(0,2)}\n`}
     }
 
-    if (answered == 10) {
-        // Resultado
+    if (answered == rightAnswers.length) {
         let result = 0;
 
-        /* for (i = 0; i < rightAnswers.length; i++) {
+        for (i = 0; i < rightAnswers.length; i++) {
             let test = document.getElementsByName('answer'+(i+1))
+            let value = '';
             for (j = 0; j < test.length; j++) {
-                if (test[j].value == rightAnswers[i]) {
-                    result += 1;
-                console.log('Right answer');
-                } else {console.log('Wrong answer');}
+                if (test[j].checked) {value = test[j].value;}
             }
-        } */
-
-        for (i = 0; i < quizAnswers.length; i++) {
-            let answer = document.getElementsByName(`answer${i+1}`);
-            if (answer[quizAnswers[i]].checked) {
+            if (value == rightAnswers[i]) {
                 result += 1;
                 console.log('Right answer');
             } else {console.log('Wrong answer');}
         }
-
         document.querySelector('#result p').innerHTML = `${result} / 10`;
         scrollTo(top);
         quizForm.style = "display:none";
         resultSection.setAttribute('class', 'appearAni');
         resultSection.style = "display:''";
+        
         } else {
             answered = 0;
             scrollTo(top);
             alert(`¡Error! No has respondido las preguntas: 
 ${notAnswered}`);
     }
+    notAnswered = '';
 });
